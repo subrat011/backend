ff86166f128583b0684ad7dcf0b7c523c72412ee
Subrat kumar Behera <subratbehera01012002@gmail.com>
2024-01-25 15:43:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req, res)=> {
+    res.send('login to twitter')
+})
+app.get('/instagram',(req, res)=> {
+    res.send('login to instagram')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
